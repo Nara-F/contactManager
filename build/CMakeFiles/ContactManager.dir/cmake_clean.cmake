db0ee95fb90e393e657632f3dc0fe1829c6ab2c2
NaraF <zhang070228@gmail.com>
2026-02-10 15:07:22 +0800

    add Cmake

---
+++ build/CMakeFiles/ContactManager.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ContactManager.dir/src/App.cpp.obj"
+  "CMakeFiles/ContactManager.dir/src/App.cpp.obj.d"
+  "CMakeFiles/ContactManager.dir/src/DataManager.cpp.obj"
+  "CMakeFiles/ContactManager.dir/src/DataManager.cpp.obj.d"
+  "CMakeFiles/ContactManager.dir/src/FileManager.cpp.obj"
+  "CMakeFiles/ContactManager.dir/src/FileManager.cpp.obj.d"
+  "CMakeFiles/ContactManager.dir/src/Service.cpp.obj"
+  "CMakeFiles/ContactManager.dir/src/Service.cpp.obj.d"
+  "CMakeFiles/ContactManager.dir/src/UserInterface.cpp.obj"
+  "CMakeFiles/ContactManager.dir/src/UserInterface.cpp.obj.d"
+  "CMakeFiles/ContactManager.dir/src/main.cpp.obj"
+  "CMakeFiles/ContactManager.dir/src/main.cpp.obj.d"
+  "ContactManager.exe"
+  "ContactManager.exe.manifest"
+  "ContactManager.pdb"
+  "libContactManager.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ContactManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
